6ecd7023fad7f4f153d2e2b07247191866bfcd68
shjunLee <30571226+shjunLee@users.noreply.github.com>
2017-08-24 18:09:25 +0800

    Add files via upload

---
+++ My Little Tools/hosts.py
@@ -0,0 +1,22 @@
+﻿import os
+import shutil
+"""
+把hosts文件从桌面移动到windows系统hosts文件指定位置
+
+"""
+
+pass_old = 'C:\\Users\\lishe\\Desktop\\hosts'
+pass_new = 'C:\\Windows\\System32\\drivers\\etc\\hosts'
+
+try:
+    shutil.move(pass_old, pass_new)
+except Exception as e:
+    print('Failed!')
+    print(e)
+else:
+    print('Move hosts successfully!')
+finally:
+    del pass_new
+    del pass_old
+
+os.system("pause")
